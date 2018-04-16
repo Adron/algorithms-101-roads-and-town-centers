e7ddf6a90dadbff72dfd96adbd1e314829c71e8f
Adron Hall <adronhall@gmail.com>
2018-04-16 09:11:36 -0700

    Switch Machines.

--- build-and-run.sh
+++ build-and-run.sh
@@ -6,18 +6,39 @@ echo "Build Project"
 
 echo "Starting Run"
 
-echo "2 \
-3 3 2 1 \
-1 2 \
-3 1 \
-2 3 \
-6 6 2 5 \
-1 3 \
-3 4 \
-2 4 \
-1 2 \
-2 3 \
-5 6 \
+echo "2
+3 3 2 1
+1 2
+3 1
+2 3
+6 6 2 5
+1 3
+3 4
+2 4
+1 2
+2 3
+5 6
 " | ./app
+
+echo "3
+6 4 2 1
+1 2
+2 3
+4 5
+5 6
+7 6 2 1
+1 2
+2 3
+3 4
+2 4
+5 6
+6 7
+9 7 3 4
+1 2
+1 5
+2 4
+2 3
+3 9
+6 8" | ./app
 
 echo "Results Correct? They should be 4, and then subsequently 12."
--- main.go
+++ main.go
@@ -1,76 +1,92 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 type nation struct {
+	cities     int
+	roads      int
+	costCenter int
+	costRoad   int
 }
 
-type city struct {
+type road struct {
+	originCity      int
+	destinationCity int
 }
 
 func main() {
 	DataRead()
 }
 
-func DataRead() ([]int64, error) {
-	var queries int64
+func DataRead() (int, error) {
 
-	_, err := fmt.Scanf("%d", &queries)
-	if err != nil {
-		return nil, err
-	}
+	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Printf("Nations: %d", queries)
+	textLine, _, _ := reader.ReadLine()
+	nations, _ := strconv.Atoi(string(textLine))
 
-	data := make([]int64, queries)
+	fmt.Printf("Nations: %d\n", nations)
 
-	for i := range data {
-		_, err := fmt.Scanf("%d", &data[i])
-		if err != nil {
-			return nil, err
+	for i := 1; i < nations+1; i++ {
+		fmt.Printf("..Processing Nation %d\n", i)
+
+		nationLineValues := getSpaceSeparatedValues(textLine, reader)
+		fmt.Println(nationLineValues)
+
+		var newNation nation
+		newNation.roads, _ = strconv.Atoi(nationLineValues[0])
+		newNation.cities, _ = strconv.Atoi(nationLineValues[1])
+		newNation.costCenter, _ = strconv.Atoi(nationLineValues[2])
+		newNation.costRoad, _ = strconv.Atoi(nationLineValues[3])
+
+		fmt.Printf("Cities: %d\nRoads: %d\nTown Center Cost: %d\nRoad Cost: %d\n",
+			newNation.cities,
+			newNation.roads,
+			newNation.costCenter,
+			newNation.costRoad)
+
+		for r := 1; r < newNation.roads+1; r++ {
+			roadLineValues := getSpaceSeparatedValues(textLine, reader)
+			var newRoad road
+			newRoad.originCity, _ = strconv.Atoi(roadLineValues[0])
+			newRoad.destinationCity, _ = strconv.Atoi(roadLineValues[1])
+
+			fmt.Printf("Road %d: City Origin %d and Destination %d.\n", r, newRoad.originCity, newRoad.destinationCity)
 		}
+		fmt.Println("- - - - - - - - -")
 	}
 
-	fmt.Println(data)
+	//for reader.ReadLine() {
+	//
+	//	fmt.Printf("Nations: %d", nations)
+	//
+	//	for i := 1; i < nations; i++ {
+	//
+	//		textLine = scanner.Text()
+	//
+	//		values := strings.Split(textLine, " ")
+	//
+	//		fmt.Printf("Nation: %s", values[0])
+	//
+	//
+	//
+	//
+	//
+	//	}
+	//	fmt.Println("Completed processing.")
+	//}
 
-	return data, nil
+	return 42, nil
 }
 
-func GetNation() {
-
+func getSpaceSeparatedValues(textLine []byte, reader *bufio.Reader) []string {
+	textLine, _, _ = reader.ReadLine()
+	values := strings.Split(string(textLine), " ")
+	return values
 }
-
-//
-//func SumTotal(list []int64) int64 {
-//	var totalSum int64 = 0
-//
-//	for _, v := range list {
-//		totalSum += v
-//	}
-//
-//	return totalSum
-//}
-
-//func DataRead() ([]int64, error) {
-//	var length int64
-//
-//	_, err := fmt.Scanf("%d", &length)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//
-//
-//
-//
-//	data := make([]int64, length)
-//
-//	for i := range data {
-//		_, err := fmt.Scanf("%d", &data[i])
-//		if err != nil {
-//			return nil, err
-//		}
-//	}
-//
-//	return data, nil
-//}
